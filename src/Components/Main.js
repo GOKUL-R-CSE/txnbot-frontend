d1d850012f2897af714f69d302ce2390c2febf3f
GOKUL-R-CSE <gokulrcseofficial@gmail.com>
2024-10-12 16:07:49 +0530

    home_page added

--- src/Components/Main.js
+++ src/Components/Main.js
@@ -1,8 +1,42 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Main = () => {
   return (
-    <div>Home Pages</div>
+    <>
+            <div>
+                <div>
+                    <div>
+                        <img src='' alt='login' />
+                    </div>
+                    <div>
+                        <h3><i></i>Account Login / Signup</h3>
+                        <div>
+                        <div>
+                            <p>Welcome to TxN_BoT</p>
+                            <p>!__ Get started with the application __!</p>
+                        </div>
+        
+                        <div></div>
+                            <div>
+                                <Link to="/login">
+                                    <a href='/login'>
+                                        Login<i></i>
+                                    </a>
+                                </Link>
+                            </div>
+                        <div>
+                                <Link to="/signup">
+                                    <a href='/signup'>
+                                        Signup<i></i>
+                                    </a>
+                                </Link>
+                        </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
   )
 }
 
