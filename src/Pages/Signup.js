efd84ab150d7c6c8e95c163b977365b98b821775
GOKUL-R-CSE <gokulrcseofficial@gmail.com>
2024-10-12 16:01:13 +0530

    authContext

--- src/Pages/Signup.js
+++ src/Pages/Signup.js
@@ -1,5 +1,5 @@
 import React, {useState} from 'react'
-import {useSignup} from '../hooks/useSignup' 
+import {useSignup} from '../Hooks/useSignup' 
 
 const Signup = () => {
     const [email, setEmail] = useState('')
